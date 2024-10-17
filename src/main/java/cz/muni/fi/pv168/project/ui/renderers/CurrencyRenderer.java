e6f6e8056c7bb73e3d52daf821b31131657b8d0e
Jan Fabšič <xfabsic@fi.muni.cz>
2024-10-17 22:51:17 +0200

    Added Renderers and ListModels for Currency and Category fields

---
+++ src/main/java/cz/muni/fi/pv168/project/ui/renderers/CurrencyRenderer.java
@@ -0,0 +1,20 @@
+package cz.muni.fi.pv168.project.ui.renderers;
+
+import cz.muni.fi.pv168.project.ui.model.Currency;
+
+import javax.swing.*;
+
+public class CurrencyRenderer extends AbstractRenderer<Currency>{
+    public CurrencyRenderer() {
+        super(Currency.class);
+    }
+
+    @Override
+    protected void updateLabel(JLabel label, Currency value) {
+        if (value != null) {
+            label.setText(value.getCurrencyCode());
+            label.setIcon(null); // TODO - resolve icons
+        }
+    }
+}
+
