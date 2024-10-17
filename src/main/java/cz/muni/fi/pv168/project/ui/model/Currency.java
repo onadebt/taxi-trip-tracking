b998eaa9e5a4a6c47c8b6c412e0e1e9f9fc8468e
Jan Fabšič <xfabsic@fi.muni.cz>
2024-10-17 22:50:27 +0200

    Added NewRide model and get/set for currency

--- src/main/java/cz/muni/fi/pv168/project/ui/model/Currency.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/model/Currency.java
@@ -4,4 +4,28 @@ public class Currency {
     private String name;
     private String currencyCode;
     private double amountPerDefault;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCurrencyCode() {
+        return currencyCode;
+    }
+
+    public void setCurrencyCode(String currencyCode) {
+        this.currencyCode = currencyCode;
+    }
+
+    public double getAmountPerDefault() {
+        return amountPerDefault;
+    }
+
+    public void setAmountPerDefault(double amountPerDefault) {
+        this.amountPerDefault = amountPerDefault;
+    }
 }
