e6b48db0e05792222b61565b66f5203e72292612
Ivan <567817@muni.cz>
2024-11-27 23:24:09 +0100

    RideTableModel deleted Icon column, Category column now renders it, distance column name changes

--- src/main/java/cz/muni/fi/pv168/project/ui/model/RideTableModel.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/model/RideTableModel.java
@@ -1,29 +1,26 @@
 package cz.muni.fi.pv168.project.ui.model;
 
 import cz.muni.fi.pv168.project.model.Category;
-import cz.muni.fi.pv168.project.model.Currency;
 import cz.muni.fi.pv168.project.model.Ride;
-import cz.muni.fi.pv168.project.model.enums.DistanceUnit;
 import cz.muni.fi.pv168.project.model.enums.TripType;
-import cz.muni.fi.pv168.project.service.RideService;
 import cz.muni.fi.pv168.project.service.interfaces.IRideService;
+import cz.muni.fi.pv168.project.ui.tabs.Settings;
 
-import javax.swing.*;
 import javax.swing.table.AbstractTableModel;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RideTableModel extends AbstractTableModel {
+public class RideTableModel extends AbstractTableModel implements EntityTableModel<Ride> {
     private List<Ride> rides;
     private final IRideService rideService;
+    private final String distanceColumnName = String.valueOf("Distance " + "("+ Settings.getDefaultDistanceUnit() + "s)");
+
 
     private final List<Column<Ride, ?>> columns = List.of(
             Column.editable("Amount currency", Double.class, Ride::getAmountCurrency, Ride::setAmountCurrency),
             Column.editable("Currency Type", String.class, Ride::getCurrencyCode, Ride::setCurrencyCode),
-            Column.editable("Distance", Double.class, Ride::getDistance, Ride::setDistance),
-            Column.editable("Distance Unit", DistanceUnit.class, Ride::getDistanceUnit, Ride::setDistanceUnit),
+            Column.editable(distanceColumnName, Double.class, Ride::getDistance, Ride::setDistance),
             Column.editable("Category", Category.class, Ride::getCategory, Ride::setCategory),
-            Column.editable("Category Icon", Icon.class, Ride::getCategoryIcon, Ride::setCategoryIcon),
             Column.editable("Trip Type", TripType.class, Ride::getTripType, Ride::setTripType),
             Column.editable("Passengers", Integer.class, Ride::getNumberOfPassengers, Ride::setNumberOfPassengers)
     );
