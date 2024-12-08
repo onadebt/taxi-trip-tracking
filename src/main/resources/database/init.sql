94c068a32aeaafb262d288a312818ac179d937a9
Ivan <567817@muni.cz>
2024-12-08 18:04:06 +0100

    init db changed

--- src/main/resources/database/init.sql
+++ src/main/resources/database/init.sql
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS "Currency"
 CREATE TABLE IF NOT EXISTS "Settings"
 (
     "id"        BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    "name"              VARCHAR(255) NOT NULL,
+    "name"              VARCHAR(255) NOT NULL UNIQUE,
     "value"             VARCHAR(255) NOT NULL,
     "createdAt"         TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -35,3 +35,21 @@ CREATE TABLE IF NOT EXISTS "Ride"
     "createdAt"     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "uuid"          VARCHAR(36) NOT NULL
 );
+
+MERGE INTO "Settings" AS target
+    USING (SELECT 'DefaultDistanceUnit' AS "name", 'KILOMETER' AS "value") AS source
+    ON target."name" = source."name"
+    WHEN NOT MATCHED THEN
+        INSERT ("name", "value") VALUES (source."name", source."value");
+
+MERGE INTO "Settings" AS target
+    USING (SELECT 'DefaultCurrency' AS "name", 'EUR' AS "value") AS source
+    ON target."name" = source."name"
+    WHEN NOT MATCHED THEN
+        INSERT ("name", "value") VALUES (source."name", source."value");
+
+MERGE INTO "Currency" AS target
+    USING (SELECT 'Euro' AS "name", 'EUR' AS "tag", 1.0 AS "rate") AS source
+    ON target."tag" = source."tag"
+    WHEN NOT MATCHED THEN
+        INSERT ("name", "tag", "rate") VALUES (source."name", source."tag", source."rate");
